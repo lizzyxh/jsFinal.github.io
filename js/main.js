6d8ded71c0c4e2b49011144f7278b10484061190
lizzyxh <laurenhidalgo@Laurens-MacBook-Pro.local>
2017-07-31 21:54:33 -0500

    InitialUpload

---
+++ js/main.js
@@ -0,0 +1,37 @@
+//Step 1:
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB3lMDcH2BZtnyvwn5k2PZJN0o6u6OGj3o",
+    authDomain: "reservation-site-e483e.firebaseapp.com",
+    databaseURL: "https://reservation-site-e483e.firebaseio.com",
+    projectId: "reservation-site-e483e",
+    storageBucket: "reservation-site-e483e.appspot.com",
+    messagingSenderId: "1026813792788"
+  };
+  firebase.initializeApp(config);
+
+
+var database = firebase.database();
+
+//Step 2: Create an empty Object Literal
+reservationData ={
+
+}
+
+//Step 3: User Data
+
+$('.reservation-day li').on('click', function(e){
+  e.preventDefault(); 
+  reservationData.day = $(this).text();
+});
+
+//Step 4:  Update the name property
+
+$('reservations').on('submit', function(e){
+  e.preventDefault(); 
+  reservationData.name = $('.reservation-name').val();
+  console.log("completed step 4");
+    var reservationDayData = database.ref('reservation-day');
+    reservationDayData.push(reservationData);
+});
+
