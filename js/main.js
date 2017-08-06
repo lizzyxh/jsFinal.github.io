bb7dde2a341ea77f41af0c7438fc7c8f8db7fe66
lizzyxh <laurenhidalgo@Laurens-MBP.home>
2017-08-06 14:37:38 -0500

    Map Styling

--- js/main.js
+++ js/main.js
@@ -74,9 +74,22 @@ getReservations();
 function initMap(){
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 40.8054491, lng: -73.9654415},
-    zoom: 14,
+    zoom: 13,
     scrollwheel: false
   });
+  var styles = [
+  {
+    {
+      stylers:[ { hue: '#9d9d9d'}, {saturation: -35} ]
+    }
+    {
+      featureType: "road"
+      elementType: 'lables',
+      stylers:[ { visibility: 'off'} ]
+    }
+  }
+
+  ];
   var marker = new google.maps.Marker({
     position: {lat: 40.8054491, lng: -73.9654415},
     map: map,
